0b73634e08f1431150ee8976c9ccf6caff84ff19
Carolina Scudeler <carolina.oliveira@bandtec.com.br>
2021-07-12 21:41:50 -0300

    Finalizing home page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  reactStrictMode: true,
+  images: {
+    domains: ["storage.googleapis.com"],
+  },
 };
-
-const withImages = require("next-images");
-module.exports = withImages();
